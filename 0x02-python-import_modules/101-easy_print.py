7d66b0a2d846bea85ed4b7fd072a4e29834e672f
keturah crony <keturahcrony@gmail.com>
2023-11-30 12:34:52 -0800

    0x02-python-import_modules

---
+++ 0x02-python-import_modules/101-easy_print.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# 101-easy_print.py
+
+exec("print('#pythoniscool')")
+
---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+# 103-fast_alphabet.py
+
+import string; print(*list(string.ascii_uppercase), sep='', end='\n')
